5a6a394dc21954f46c85db2dc48cb727fc9f8bb8
codeftshaunak <sagardeyyash19@gmail.com>
2025-09-20 17:37:33 +0600

    fix the agency title

--- src/components/AgencyUI/AgencyTitle.tsx
+++ src/components/AgencyUI/AgencyTitle.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-
 import { RiEdit2Fill } from "react-icons/ri";
 import { FiPlus } from "react-icons/fi";
 // import { AgencyUpdatedModal } from "./ProfileUpdated";
@@ -18,40 +17,30 @@ const AgencyTitle = ({
 
   return (
     <>
-      <div className="flex flex-row items-center className="mb-[0.5rem] mt-[1rem]">
-        <span : "1.3rem"
-          }
-         className="mb-[0] font-semibold">
+      <div className="flex flex-row items-center mt-4 mb-2">
+        <span className={`font-semibold ${isSmall ? "text-sm" : "text-lg"}`}>
           {children}
         </span>
+
         {isValue && (
-          <div className="className= flex flex-col backgroundColor= cursor-pointer"rounded w-[20px] border h-[20px] items-center justify-center"
-            transition="0.6s ease-in-out"
-            _hover={{
-              border: "2px solid var(--primarycolor)",
-              backgroundColor: "transparent",
-              color: "var(--primarycolor)",
-            }}
+          <div
+            className="flex items-center justify-center w-5 h-5 ml-2 transition duration-200 border rounded cursor-pointer hover:border-primary hover:bg-transparent hover:text-primary"
             onClick={() => setIsModal(true)}
           >
-            <RiEdit2Fill />
+            <RiEdit2Fill size={14} />
           </div>
         )}
-        {noAdded ||
-          (!isValue && (
-            <div className="className= flex flex-col backgroundColor= cursor-pointer"rounded w-[30px] border h-[30px] items-center justify-center"
-              transition="0.6s ease-in-out"
-              _hover={{
-                border: "2px solid var(--primarycolor)",
-                backgroundColor: "transparent",
-                color: "var(--primarycolor)",
-              }}
-              onClick={() => setIsModal(true)}
-            >
-              <FiPlus />
-            </div>
-          ))}
+
+        {!isValue && noAdded && (
+          <div
+            className="flex items-center justify-center ml-2 transition duration-200 border rounded cursor-pointer h-7 w-7 hover:border-primary hover:bg-transparent hover:text-primary"
+            onClick={() => setIsModal(true)}
+          >
+            <FiPlus size={16} />
+          </div>
+        )}
       </div>
+
       {/* {isModal && (
         <AgencyUpdatedModal
           isModal={isModal}
