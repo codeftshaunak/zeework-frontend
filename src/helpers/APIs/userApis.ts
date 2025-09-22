7631bf032a0b8b30d8cb23b6064816a72d700340
codeftshaunak <sagardeyyash19@gmail.com>
2025-09-22 12:38:23 +0600

    user api fixes

--- src/helpers/APIs/userApis.ts
+++ src/helpers/APIs/userApis.ts
@@ -1,18 +1,35 @@
+import { AxiosError, AxiosRequestConfig, Method } from "axios";
 import { handleApiError } from "./common";
 import { API } from "./proxy";
 
-  const headers = {
-    token: authtoken,
+interface ApiRequestOptions {
+  endpoint: string;
+  method?: Method;
+  data?: any;
+  authToken?: string | null;
+  customHeaders?: Record<string, string>;
+}
+
+export const apiRequest = async <T = any>({
+  endpoint,
+  method = "GET",
+  data,
+  authToken,
+  customHeaders = {},
+}: ApiRequestOptions): Promise<T> => {
+  const headers: Record<string, string> = {
+    ...(authToken ? { token: authToken } : {}),
     ...customHeaders,
   };
 
-  if (!(data instanceof FormData)) {
-    headers["Content-Type"] = "application/json";
-  } else {
+  // Set content type automatically
+  if (data instanceof FormData) {
     headers["Content-Type"] = "multipart/form-data";
+  } else {
+    headers["Content-Type"] = "application/json";
   }
 
-  const config = {
+  const config: AxiosRequestConfig = {
     method,
     url: endpoint,
     data,
@@ -20,31 +37,11 @@ import { API } from "./proxy";
   };
 
   try {
-    const response = await API(config);
+    const response = await API.request<T>(config);
     return response.data;
   } catch (error) {
-    // Use the error handling hook
-    // const { handleApiError } = useApiErrorHandling();
-    handleApiError(error);
-    return error.response.data;
+    const err = error as AxiosError;
+    const handled = handleApiError(err);
+    throw handled; // rethrow so caller knows it's an error
   }
 };
-
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-export;
-
-// Placeholder exports to fix build
-export const placeholder = true;
